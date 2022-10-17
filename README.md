592909ff41544e38d91e9327db61a729237d6d86
david-roper <davidroper321@gmail.com>
2022-10-17 01:04:30 -0400

    fixed help bugs and finished assignment

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Comp445
 Lab assignments of Comp445
+
+
+# How to run
+1. Navigate to the directory containing httpc.py using the ```cd``` command.
+2. Run the script with any commands defined in the assignment
+
+```python httpc.py [command]```
+
+Example:
+
+```python httpc.py get 'http://httpbin.org/get?course=networking&assignment=1'```
--- httpc.py
+++ httpc.py
@@ -5,6 +5,8 @@ import re
 from urllib.parse import urlparse, parse_qs
 from json import dumps
 
+#Code created by David Roper (40131739) and Gianfranco Dumoulin (40097768)
+
 HELP_GET = """usage: httpc get [-v] [-h key:value] URL 
 Get executes a HTTP GET request for a given URL. 
  -v Prints the detail of the response such as protocol, status, and headers. 
@@ -18,7 +20,6 @@ file.
  -d string Associates an inline data to the body HTTP POST request.
  -f file Associates the content of a file to the body HTTP POST request.
 Either [-d] or [-f] can be used but not both.
-
 """
 
 HELP_GENERAL = """httpc is a curl-like application but supports HTTP protocol only.
@@ -30,46 +31,43 @@ The commands are:
     help prints this screen.
 Use "httpc help [command]" for more information about a command."""
 
+def print_help(request):
+    if not request:
+        request = ''
+    if help:
+    # if len(help) > 1:
+        if request.lower() == 'get':
+            print(HELP_GET)
+            return
+        elif request.lower() == 'post':
+            print(HELP_POST)
+            return
+        else:
+            # general help with no arguments
+            print(HELP_GENERAL)
+            return
+    # print('url', url)
+
+
 def run_client(
     request,
-    url: str, 
+    url, 
     data, 
     infile, 
     outfile, 
     headers,
-    help, 
     verbose,
-    port,
-    parser: argparse.ArgumentParser
+    port
 ):
-    if help:
-        if len(help) > 1:
-            if help[1].lower() == 'get':
-                print(HELP_GET)
-            elif help[1].lower() == 'post':
-                print(HELP_POST)
-        else:
-            # general help with no arguments
-            print(HELP_GENERAL)
-            pass
-        return
     if not url:
         print('Please specify a url.')
         return
-    # if 'http:' not in url or 'https:' not in url:
-    #     url = 'http://' + url
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     try:
-        # print("connected")
-        
-        # print('request:', request)
-        print(args)
         parsed_url = urlparse(url)
         host = parsed_url.hostname
-        print('host:', host)
         path = parsed_url.path
-        print('path:', path)
         query_params = parsed_url.query
         conn.connect((host, port))
 
@@ -107,49 +105,72 @@ def run_client(
                 break
             response = response + chunk
        
-        
-        # sys.stdout.write("request recieved \n")
         decoded_resp = response.decode("utf-8")
         if verbose:
             sys.stdout.write("Output: \n" + decoded_resp)
         else:
-            non_verbose_resp = decoded_resp.split('\r\n\r\n')
-            sys.stdout.write("\n" + non_verbose_resp[1])
-        with open('response.txt', 'w') as ftw:
-            ftw.write(decoded_resp)
+            decoded_resp = decoded_resp.split('\r\n\r\n')[1]
+            sys.stdout.write("\n" + decoded_resp)
+        if outfile:
+            with open(outfile, 'w') as ftw:
+                ftw.write(decoded_resp)
     finally:
         conn.close()
 
 
-# Usage: python httpc.py --host host --port port
-# example: python httpc.py --host httpbin.org --port 80
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--get", help="""usage: httpc get [-v] [-h key:value] URL 
-# Get executes a HTTP GET request for a given URL.\n 
-# -v Prints the detail of the response such as protocol, status, and headers. \n
-# -h key:value Associates headers to HTTP Request with the format 'key:value'.""",type=str, default="httpbin.org/get?status=418")
 
 parser = argparse.ArgumentParser(description='httpc is a curl like application that supports http get and post requests.', add_help=False)
-parser.add_argument('help', nargs = '*', help='Used to provide info on httpc functionality', default = '')
-parser.add_argument('request', choices=['get', 'post', 'GET', 'POST'], nargs = '?', help='HTTP request method, the option are the following: \n get \n post')
-parser.add_argument('url', help='URL', nargs = '?')
-parser.add_argument('--d', dest='data', help='payload data to post', default=None)
-parser.add_argument('-f', dest='infile', help='file containing POST data',default=None)
-parser.add_argument('-o', dest='outfile', help='file to store response',default=None)
-parser.add_argument('-h', dest='headers', action='append', help='HTTP headers to include',default=None)
-parser.add_argument('-v', dest='verbose', action='store_true', help='prints verbose output', default=False)
-parser.add_argument("--port", help="server port to connect to", type=int, default=80)
+subparsers = parser.add_subparsers(help='sub-command help')
+
+# help parser
+help_parser = subparsers.add_parser('help', help='Used to provide info on httpc functionality', add_help=False)
+help_parser.set_defaults(which='help')
+help_parser.add_argument('request', choices = ['get', 'post', 'GET', 'POST', ''], nargs = '?')
+
+# GET parser
+get_parser = subparsers.add_parser('get', help='HTTP get method, the option are the following: \n get \n post', add_help=False)
+get_parser.set_defaults(which='get')
+get_parser.add_argument('url', help = 'URL')
+
+#POST parser
+post_parser = subparsers.add_parser('post', help='HTTP post method, the option are the following: \n get \n post', add_help=False)
+post_parser.set_defaults(which='post')
+post_parser.add_argument('url', help = 'URL')
+
+data_options = {'dest':'data', 'help':'payload data to post', 'default':None}
+infile_options = {'dest':'infile', 'help':'file containing POST data','default':None}
+outfile_options = {'dest':'outfile', 'help':'file to store response','default':None}
+header_options = {'dest':'headers', 'action':'append', 'help':'HTTP headers to include','default':None}
+verbose_option = {'dest':'verbose', 'action':'store_true', 'help':'prints verbose output', 'default':False}
+port_option = {'help':"server port to connect to", 'type':int, 'default':80}
+
+get_parser.add_argument('--d', **data_options)
+get_parser.add_argument('-f', **infile_options)
+get_parser.add_argument('-o', **outfile_options)
+get_parser.add_argument('-h',**header_options)
+get_parser.add_argument('-v',**verbose_option)
+get_parser.add_argument('--port',**port_option)
+
+
+post_parser.add_argument('--d', **data_options)
+post_parser.add_argument('-f', **infile_options)
+post_parser.add_argument('-o', **outfile_options)
+post_parser.add_argument('-h', **header_options)
+post_parser.add_argument('-v', **verbose_option)
+post_parser.add_argument('--port', **port_option)
+
 args = parser.parse_args()
-# print(args)
-run_client(
-    args.request,
-    args.url,
-    args.data,
-    args.infile,
-    args.outfile,
-    args.headers,
-    args.help,
-    args.verbose,
-    args.port,
-    parser
-)
+
+if args.which == 'help':
+    print_help(args.request)
+else:
+    run_client(
+        args.which,
+        args.url,
+        args.data,
+        args.infile,
+        args.outfile,
+        args.headers,
+        args.verbose,
+        args.port
+    )
